e830571c546260376099d47abd75d68f14dc243c
dem321 <dab.work.en@gmail.com>
2023-09-28 23:14:19 +0500

    Added models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Employee, Dish, Order, DishOrder
+
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Dish)
+admin.site.register(Order)
+admin.site.register(DishOrder)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+from django.core.mail import send_mail
+from django.utils import timezone
+
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField('Имя', max_length=30)
+    second_name = models.CharField('Фамилия', max_length=30)
+    middle_name = models.CharField('Отчество', max_length=30)
+    
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+class Dish(models.Model):
+    name = models.CharField('', max_length=30)
+    components = models.CharField('Состав', max_length=30)
+    price = models.IntegerField('Цена')
+    picture = models.ImageField('Изображение')
+
+    class Meta:
+        verbose_name = 'Блюдо'
+        verbose_name_plural = 'Блюда'
+
+class Order(models.Model):
+    date = models.DateTimeField('Дата заказа')
+    employee = models.ForeignKey(Employee, models.CASCADE, verbose_name='Сотрудник')
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+class DishOrder(models.Model):
+    order = models.ForeignKey(Order, models.CASCADE, verbose_name='Заказ')
+    dish = models.ForeignKey(Dish, models.CASCADE, verbose_name='Блюдо')
+
+    class Meta:
+        verbose_name = 'БлюдоЗаказа'
+        verbose_name_plural = 'БлюдоЗаказа'
+
